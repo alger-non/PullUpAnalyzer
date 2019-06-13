c35cc6c0105cceb9770957945274f204044bf4f4
chamecall <16it3.kakoshka.y@pdu.by>
2019-06-13 23:34:23 +0300

    got rid of magic numbers

--- Drawer.py
+++ Drawer.py
@@ -1,35 +1,42 @@
 import cv2
-import skcuda
 
 
 class Drawer:
+    YELLOW_COLOR = (0, 255, 255)
+    RED_COLOR = (0, 0, 255)
+    BLUE_color = (255, 0, 0)
+    GREEN_COLOR = (0, 255, 0)
+    BLACK_COLOR = (0, 0, 0)
+
     def __init__(self):
         pass
 
     @staticmethod
-    def draw_skeleton(frame, points, pose_pairs):
-        line_color = (0, 255, 255)
+    def draw_skeleton(frame, points, pose_pairs, line_color=YELLOW_COLOR, circle_color=RED_COLOR, radius=8,
+                      line_thickness=3):
 
         for pair in pose_pairs:
             part_a = pair[0]
             part_b = pair[1]
 
             if points[part_a] and points[part_b]:
-                cv2.line(frame, points[part_a], points[part_b], line_color, 3, lineType=cv2.LINE_AA)
-                cv2.circle(frame, points[part_a], 8, (0, 0, 255), thickness=-1, lineType=cv2.FILLED)
-                cv2.circle(frame, points[part_b], 8, (0, 0, 255), thickness=-1, lineType=cv2.FILLED)
+                cv2.line(frame, points[part_a], points[part_b], line_color, line_thickness, lineType=cv2.LINE_AA)
+                cv2.circle(frame, points[part_a], radius, circle_color, thickness=-1, lineType=cv2.FILLED)
+                cv2.circle(frame, points[part_b], radius, circle_color, thickness=-1, lineType=cv2.FILLED)
 
     @staticmethod
-    def draw_numbered_joints(frame, points: dict, needed_points: dict):
+    def draw_numbered_joints(frame, points: dict, needed_points: dict, circle_color=YELLOW_COLOR, text_color=RED_COLOR,
+                             radius=8, thickness=2, font_scale=1):
         for joint, point in points.items():
             if not point:
                 break
             joint_number = needed_points[joint]
-            cv2.circle(frame, (point[0], point[1]), 8, (0, 255, 255), thickness=-1, lineType=cv2.FILLED)
-            cv2.putText(frame, f'{joint_number}', (point[0], point[1]), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2,
+            cv2.circle(frame, (point[0], point[1]), radius, circle_color, thickness=-1, lineType=cv2.FILLED)
+            cv2.putText(frame, f'{joint_number}', (point[0], point[1]), cv2.FONT_HERSHEY_SIMPLEX, font_scale,
+                        text_color, thickness,
                         lineType=cv2.LINE_AA)
 
     @staticmethod
-    def print_message(frame, message, x, y):
-        cv2.putText(frame, message, (x, y), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2,
+    def print_message(frame, message, x, y, text_color=RED_COLOR, thickness=2, font_scale=1):
+        cv2.putText(frame, message, (x, y), cv2.FONT_HERSHEY_SIMPLEX, font_scale, text_color, thickness,
                     lineType=cv2.LINE_AA)
