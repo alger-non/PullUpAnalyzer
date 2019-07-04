522a5473c4a6ec8ce029443c19dedbcf2cc8affc
algernon <16it3.kakoshka.y@pdu.by>
2019-07-04 15:00:37 +0300

    moved video/audio tools to VideoProcessor

--- PullUpCounter.py
+++ PullUpCounter.py
@@ -1,9 +1,7 @@
 import sys
 from sys import platform
-from AudioProcessor import AudioProcessor
 from PhaseQualifier import PhaseQualifier
 from VideoProcessor import VideoProcessor
-import cv2
 import os
 import argparse
 import time
@@ -13,13 +11,14 @@ dir_path = os.path.dirname(os.path.realpath(__file__))
 
 class PullUpCounter:
     """The general class for launching the pull ups counter."""
+
     def __init__(self):
         self.input_file = ""
         self.short_input_filename = ""
         self.output_dir = ""
         self.json_dir = ""
         self.use_raw_data = False
-        self.cap = None
+
         self.video_writer = None
         self.audio_writer = None
         self.required_points = {"Nose": 0, "Neck": 1, "RShoulder": 2, "RElbow": 3, "RWrist": 4, "LShoulder": 5,
@@ -36,6 +35,7 @@ class PullUpCounter:
 
         self.pose_processor = PhaseQualifier(30, 30, 5, 0.5)
         self.video_processor = None
+        self.output_file_name = ""
         self.parse_cmd_line()
 
     def parse_cmd_line(self):
@@ -52,14 +52,16 @@ class PullUpCounter:
         self.input_file = args.input_file
         if not os.path.isfile(self.input_file):
             raise FileNotFoundError("Input file not found.")
-        self.short_input_filename = os.path.basename(self.input_file).split('.')[0]
+        self.short_input_filename = os.path.basename(self.input_file)
+        print(self.short_input_filename)
         if self.use_raw_data:
-            self.json_dir = self.find_json_dir_by_video_name(args.input_file)
+            self.json_dir = self.find_json_dir_by_video_name(self.input_file)
             if not self.json_dir:
                 raise FileNotFoundError("Json directory not found in the video's specified directory")
         self.output_dir = args.output_dir
         if not os.path.isdir(self.output_dir):
             raise FileNotFoundError("Output directory not found.")
+        self.output_file_name = os.path.join(self.output_dir, f'{self.short_input_filename}')
 
     def find_json_dir_by_video_name(self, filename):
         """Find a directory consisting json files by video name.
@@ -68,46 +70,21 @@ class PullUpCounter:
         having the same name which the video has.
         """
         par_dir = os.path.dirname(filename)
-        possible_json_dir = f'{os.path.join(par_dir, self.short_input_filename)}_json'
+        possible_json_dir = f'{os.path.join(par_dir, self.short_input_filename.split(".")[0])}_json'
         return possible_json_dir if os.path.isdir(possible_json_dir) else None
 
-    def create_video_capture(self):
-        self.cap = cv2.VideoCapture(self.input_file)
-
-    def create_video_writer(self):
-        cap_width = int(self.cap.get(cv2.CAP_PROP_FRAME_WIDTH))
-        cap_height = int(self.cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
-        fps = self.cap.get(cv2.CAP_PROP_FPS)
-
-        output_filename_without_sound = os.path.join(self.output_dir, f'{self.short_input_filename}_without_audio.avi')
-        self.video_writer = cv2.VideoWriter(output_filename_without_sound, cv2.VideoWriter_fourcc(*"XVID"), fps,
-                                            (cap_width, cap_height))
-
-    def add_audio(self):
-        self.audio_writer.add_background_audio()
-
-    def create_audio_writer(self):
-        output_filename_without_sound = os.path.join(self.output_dir, f'{self.short_input_filename}_without_audio.avi')
-        output_filename_with_sound = os.path.join(self.output_dir, f'{self.short_input_filename}.avi')
-        self.audio_writer = AudioProcessor(self.input_file, output_filename_without_sound, output_filename_with_sound)
-
     def create_video_processor(self):
-        self.video_processor = VideoProcessor(self.cap, self.pose_processor, self.required_points, self.required_pairs)
+        self.video_processor = VideoProcessor(self.input_file, self.output_file_name, self.pose_processor,
+                                              self.required_points, self.required_pairs)
 
     def start(self):
         """Launch the pull up counter in the way depending on the chosen method."""
-        self.create_video_capture()
-        self.create_video_writer()
         self.create_video_processor()
-        self.create_audio_writer()
 
         if self.use_raw_data:
             self.exec_with_raw_data()
         else:
             self.exec()
-        self.release_video_cap()
-        self.release_video_writer()
-        cv2.destroyAllWindows()
 
     def exec(self):
         """Process the input video using the OpenPose library."""
@@ -134,7 +111,6 @@ class PullUpCounter:
                 ' in CMake and have this Python script in the right folder?')
             raise e
 
-
         params = dict()
         params["model_folder"] = "models/"
         params['number_people_max'] = 1
@@ -144,45 +120,14 @@ class PullUpCounter:
         op_wrapper.configure(params)
         op_wrapper.start()
 
-        for processed_frame in self.video_processor.process_video_with_net(self.cap, op_wrapper):
-            self.show_processed_frame(processed_frame)
-            self.write_frame_to_output(processed_frame)
-            if cv2.waitKey(1) > 0:
-                break
+        self.video_processor.process_video_with_net(op_wrapper)
 
     def exec_with_raw_data(self):
         """Process the input file using prepared json-files."""
-        for processed_frame in self.video_processor.process_video_with_raw_data(self.cap, self.json_dir):
-            self.show_processed_frame(processed_frame)
-            self.write_frame_to_output(processed_frame)
-            if cv2.waitKey(1) > 0:
-                break
-
-    def release_video_cap(self):
-        if self.cap:
-            self.cap.release()
-
-    def add_audio_events(self):
-        for rep_time, is_clean_rep in self.video_processor.events_labels:
-            event = 'Complete' if is_clean_rep else 'Fail'
-            self.audio_writer.add_event(event, rep_time)
-
-    def release_video_writer(self):
-        if self.video_writer:
-            self.video_writer.release()
-
-    @staticmethod
-    def show_processed_frame(frame):
-        cv2.imshow('Output', frame)
-
-    def write_frame_to_output(self, frame):
-        self.video_writer.write(frame)
+        self.video_processor.process_video_with_raw_data(self.json_dir)
 
 
 pull_up_counter = PullUpCounter()
 t = time.time()
 pull_up_counter.start()
-pull_up_counter.add_audio_events()
-pull_up_counter.add_audio()
-del pull_up_counter.audio_writer
 print(f'Execution time: {time.time() - t:.3} sec')
--- VideoProcessor.py
+++ VideoProcessor.py
@@ -5,13 +5,18 @@ import json
 from ResultsDrawer import ResultsDrawer
 from os import walk
 from PhaseQualifier import PhaseQualifier
+from AudioProcessor import AudioProcessor
 
 
 class VideoProcessor:
     """Class to handle an input video."""
-    def __init__(self, cap: cv2.VideoCapture, phase_definer: PhaseQualifier, required_points, required_pairs):
-        self.cap = cap
-        self._fps = int(cap.get(cv2.CAP_PROP_FPS))
+
+    def __init__(self, input_file_name, output_file_name_with_sound, phase_definer: PhaseQualifier, required_points,
+                 required_pairs):
+        self.output_file_name_with_sound = output_file_name_with_sound
+        self.input_file_name = input_file_name
+        self.cap = cv2.VideoCapture(self.input_file_name)
+        self._fps = int(self.cap.get(cv2.CAP_PROP_FPS))
         self.phase_qualifier = phase_definer
         self.required_points = required_points
         self.required_pairs = required_pairs
@@ -21,31 +26,31 @@ class VideoProcessor:
         self._prev_unclean_reps_amount = 0
         self._frame_num = 0
 
-    def process_video_with_net(self, cap: cv2.VideoCapture, op_wrapper):
-        """Create a generator returning processed frames using only an input video.
+        cap_width = int(self.cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+        cap_height = int(self.cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+        fps = self.cap.get(cv2.CAP_PROP_FPS)
 
-        :param cap: an initialized video-capture instance to read frames from a video
-        :param op_wrapper: an initialized open-pose instance to handle frames
-        :return: a generator returning processed frames
-        """
-        from openpose import pyopenpose as op
-        # pass _fps to the ResultsDrawer instance to obtain same animation effect on videos with different _fps
-        self._drawer = ResultsDrawer(self._fps)
-        while True:
-            has_frame, frame = cap.read()
-            if not has_frame:
-                return None
-            self._frame_num += 1
-            datum = op.Datum()
-            datum.cvInputData = frame
-            op_wrapper.emplaceAndPop([datum])
-            # check whether datum contains person key points (25 points consisting of x, y, probability)
-            if datum.poseKeypoints.size == 75:
-                points = Utils.extract_required_points(datum.poseKeypoints[0], self.required_points)
-                self.phase_qualifier.qualify_state(points)
-                self._update_reps_time_labels()
-                frame = self.put_info_on_frame(frame, points)
-            yield frame
+        print(self.output_file_name_with_sound)
+        output_file_name, output_file_extension = self.output_file_name_with_sound.split('.')
+        self.output_file_name_without_sound = f'{output_file_name}_without_audio.{output_file_extension}'
+        self.video_writer = cv2.VideoWriter(self.output_file_name_without_sound, cv2.VideoWriter_fourcc(*"XVID"), fps,
+                                            (cap_width, cap_height))
+        self.audio_writer = None
+
+    def create_audio_writer(self):
+        self.audio_writer = AudioProcessor(self.input_file_name, self.output_file_name_without_sound,
+                                           self.output_file_name_with_sound)
+
+    def delete_audio_writer(self):
+        del self.audio_writer
+
+    @staticmethod
+    def show_processed_frame(frame):
+        cv2.imshow('Output', frame)
+        cv2.waitKey(1)
+
+    def write_frame_to_output(self, frame):
+        self.video_writer.write(frame)
 
     @staticmethod
     def get_json_files_from_dir(json_dir):
@@ -61,10 +66,47 @@ class VideoProcessor:
         json_files = sorted(json_files)
         return json_files
 
-    def process_video_with_raw_data(self, cap: cv2.VideoCapture, json_dir):
+    def release_video_cap(self):
+        if self.cap:
+            self.cap.release()
+
+    def release_video_writer(self):
+        if self.video_writer:
+            self.video_writer.release()
+
+    def release_video_tools(self):
+        cv2.destroyAllWindows()
+        self.release_video_cap()
+        self.release_video_writer()
+
+    def process_video_with_net(self, op_wrapper):
+        """Create a generator returning processed frames using only an input video.
+
+        :param op_wrapper: an initialized open-pose instance to handle frames
+        :return: a generator returning processed frames
+        """
+        from openpose import pyopenpose as op
+        # pass _fps to the ResultsDrawer instance to obtain same animation effect on videos with different _fps
+        self._drawer = ResultsDrawer(self._fps)
+        while True:
+            has_frame, frame = self.cap.read()
+            if not has_frame:
+                break
+            self._frame_num += 1
+            datum = op.Datum()
+            datum.cvInputData = frame
+            op_wrapper.emplaceAndPop([datum])
+            # check whether datum contains person key points (25 points consisting of x, y, probability)
+            if datum.poseKeypoints.size == 75:
+                points = Utils.extract_required_points(datum.poseKeypoints[0], self.required_points)
+                frame = self.handle_points(frame, points)
+            self.handle_frame(frame)
+        self.release_video_tools()
+        self.overlay_audio()
+
+    def process_video_with_raw_data(self, json_dir):
         """Create a generator returning processed frames using an input video and json key points.
 
-        :param cap: an initialized video-capture instance to read frames from a video
         :param json_dir: a directory name containing json files with key points
         :return: a generator returning processed frames
         """
@@ -75,18 +117,33 @@ class VideoProcessor:
             full_filename_path = os.path.join(json_dir, filename)
             with open(full_filename_path, "r") as json_data:
                 data = json.load(json_data)
-                has_frame, frame = cap.read()
+                has_frame, frame = self.cap.read()
                 if not has_frame:
-                    return None
+                    return
                 self._frame_num += 1
                 # check whether the json file contains person key points (is a person found?)
                 if data['people']:
                     points_list = data['people'][0]['pose_keypoints_2d']
                     points = Utils.extract_required_json_points(points_list, self.required_points)
-                    self.phase_qualifier.qualify_state(points)
-                    self._update_reps_time_labels()
-                    frame = self.put_info_on_frame(frame, points)
-                yield frame
+                    frame = self.handle_points(frame, points)
+                self.handle_frame(frame)
+        self.release_video_tools()
+        self.overlay_audio()
+
+    def overlay_audio(self):
+        self.create_audio_writer()
+        self.create_audio_events()
+        self.put_audio_on_video()
+        self.delete_audio_writer()
+
+    def handle_points(self, frame, points):
+        self.phase_qualifier.qualify_state(points)
+        self._update_reps_time_labels()
+        return self.put_info_on_frame(frame, points)
+
+    def handle_frame(self, frame):
+        self.show_processed_frame(frame)
+        self.write_frame_to_output(frame)
 
     def _update_reps_time_labels(self):
         if self._prev_clean_reps_amount != self.phase_qualifier.clean_repeats:
@@ -108,3 +165,11 @@ class VideoProcessor:
         self._drawer.draw_line_between_wrists(new_frame, points)
         self._drawer.draw_chin_point(new_frame, self.phase_qualifier)
         return new_frame
+
+    def create_audio_events(self):
+        for rep_time, is_clean_rep in self.events_labels:
+            event = 'Complete' if is_clean_rep else 'Fail'
+            self.audio_writer.add_event(event, rep_time)
+
+    def put_audio_on_video(self):
+        self.audio_writer.add_background_audio()
